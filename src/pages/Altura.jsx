93857fe214af1c3ca1545199d1f36d63df9d17b8
SINAP <zjoao4529@gmail.com>
2026-03-26 23:34:47 -0300

    tem problemas em altura e header(css)

--- src/pages/Altura.jsx
+++ src/pages/Altura.jsx
@@ -1,17 +1,65 @@
-import React from "react";
-import "../styles/Content.css";
-import { useState } from "react";
+import { React, useState } from "react";
+import "../styles/Altura.css";
+
 export default function Altura() {
-       const [velocidade, setVelocidade] = useState(20) ;
-    return (
-        <>
-        <div className="Conteudo">
-        <select onChange={(e) => setVelocidade(e.target.value)}>
-                    <option value='20'>20m/s</option>
-                    <option value="30">30m/s</option>
-                    <option value="40m">40ms</option>
-                </select><br />
+       const [velocidade, setVelocidade] = useState("") ;
+       const [angulo, setAngulo ] = useState("");
+       const [altura, setAltura] = useState(null);
+
+       const calcularAltura = () => {
+        const v0 = parseFloat(velocidade);
+        const ang = parseFloat(angulo);
+
+        if (isNaN(v0) || isNaN(ang)) {
+            alert("digite valores validos!")
+            return;
+        }
+        
+        // nao esta funcionando esse IF (nao sei o porque, alguem em ajuda por favor)
+        if (ang > 90 && ang > 25) {
+            alert("o angulo tem que ser menor que 91° e maior que 24°")
+            return;
+        }
+
+        if ( v0 > 30 ) {
+            alert("A velocidade inicial tem que ser menor que 31 m/s")
+            return;
+        }
+
+        const g = 9.8;
+
+        // converte graus para radianos
+        const rad = (ang * Math.PI) / 180;
+
+        // formula da altura maxima
+        const h = (v0 ** 2 * Math.sin(rad) ** 2) / (2 * g);
+
+        setAltura(h.toFixed(2));
+       };
+
+       return(
+        <div className="container">
+            <h2>Altura Maxima do Lançamento oblíquo</h2>
+
+            <input 
+            type="number" 
+            placeholder="velocidade inicial (m/s)"
+            value={velocidade}
+            onChange={(e) => setVelocidade(e.target.value)}
+            />
+
+            <input 
+            type="number"
+            placeholder="Ângulo (graus)"
+            value={angulo}
+            onChange={(e) => setAngulo(e.target.value)}
+            />
+
+            <button onClick={calcularAltura}>Calcular</button>
+
+            {altura !== null && (
+                <p>Altura Máxima: {altura}m </p>
+            )}
         </div>
-        </>
-    )
+       )
 }
