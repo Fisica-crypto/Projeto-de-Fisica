afa27a698d9e0599154c2e139cca76c3a4a943a7
Bruno V <vitalb@acad.ifma.edu.br>
2026-04-07 11:49:21 -0300

    ..

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,38 @@
-import React from "react";
-import "../styles/Home.css"
+import "../styles/Home.css";
 
-export default function Home(){
-    return(
-        <div className="corpo">
-            <h1>Lançamento Obliquo</h1>
+export default function Home() {
+  return (
+    <div className="corpo">
+      <div id="input">
+        <div>
+          <span>Valor do ângulo:</span>
+          <input type="text" />
         </div>
-    )
+        <div>
+          <span>Gravidade:</span>
+          <input type="text" />
+        </div>
+        <div>
+          <span>Velocidade:</span>
+          <input type="text" />
+        </div>
+      </div>
+      <div id="container-results">
+        <div id="left">
+          <div>
+            <span>Altura:</span>
+          </div>
+          <div>
+            <span>Alcance:</span>
+          </div>
+          <div>
+            <span>Tempo:</span>
+          </div>
+        </div>
+        <div id="right">
+          <h1>ANIMAÇÃO</h1>
+        </div>
+      </div>
+    </div>
+  );
 }
